7f92eaaa1e3109fa15f1c8bb12cc19331a28f877
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2024-06-30 19:16:00 +0700

    fix

--- src/Components/DataHome.jsx
+++ src/Components/DataHome.jsx
@@ -24,9 +24,7 @@ function DataHome() {
             loading ? "opacity-0 -translate-x-32" : "opacity-100 translate-x-0"
           }`}
         >
-          <h3 className="font-bold text-xs md:text-sm">
-            Hi, I am Tri Saptono test
-          </h3>
+          <h3 className="font-bold text-xs md:text-sm">Hi, I am Tri Saptono</h3>
           <h1 className="font-bold text-xl md:text-3xl">
             Junior Web Developer
             <span className="text-blue-600 text-2xl md:text-4xl ml-2">.</span>
